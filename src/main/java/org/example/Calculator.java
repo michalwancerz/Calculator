bea3856ad822eab18837d932dc649a73569bff9c
michalwancerz <wancerzm@gmail.com>
2023-06-05 20:02:55 +0200

    dodawanie klasy calkulator dodawanie odejmowanie mnozenie

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,8 +2,20 @@ package org.example;
 
 public class Calculator {
 
-    public int add (int x, int y) {
+    public double add(double x, double y) {
         return x + y;
+    }
+
+    public double sub(double x, double y) {
+        return x - y;
+    }
+
+    public double multi(double x, double y) {
+        return x * y;
+    }
+
+    public double division (double x, double y) {
+        return x / y;
 
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(4,105));
+        System.out.println(calculator.sub(6, 5.5));
+        System.out.println(calculator.multi(2, 8));
+        System.out.println(calculator.division(10, 5));
     }
 }
